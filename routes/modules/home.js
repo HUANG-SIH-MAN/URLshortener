75b32afaae03eada7fa43b11238ff6dde50953eb
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-09-29 00:44:32 +0800

    feat:驗證是否重複輸入相同網址，如果是回傳相同的短網址

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -8,14 +8,24 @@ router.get('/', (req, res) => {
     res.render('index')
 })
 
-router.post('/result', (req, res) => {
-    const shortURL = createShortURL()
-    URL.create({
-        orignURL: req.body.URL, 
-        shortURL: shortURL,
+router.post('/result', (req, res) => { 
+    URL.findOne({ orignURL: req.body.URL })
+    .lean()
+    .select('shortURL')
+    .then((newURL) => {
+        let shortURL = ''
+            if (!newURL) {
+                shortURL = createShortURL()
+                URL.create({
+                    orignURL: req.body.URL, 
+                    shortURL: shortURL,
+                })
+            } else {
+                shortURL = newURL.shortURL
+            }
+        return res.render('result', { shortURL: `http://localhost:3000/${ shortURL }` })
     })
-    res.render('result', { shortURL: `http://localhost:3000/${shortURL}` })
-
+    .catch(error => console.log(error))
 })
 
 router.get('/:shortURL', (req, res) => {
