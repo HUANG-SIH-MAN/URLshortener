b10563f5e1df1cc2bd9a6a9bee1ad875d0afca08
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-09-28 13:47:47 +0800

    feat:新增縮短網址功能(未加入檢查機制，檢差是否有重複網址輸入以及重複的短網址產生)

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const exphbs  = require('express-handlebars') 
+const bodyParser = require('body-parser')
 const app = express()
 const routes = require('./routes')
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
+require('./config/mongoose')
 app.use(routes)
 
 app.listen(port, ()=>{
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const createShortURL = require('../../public/javascript/createShortURL')
+
+const URL = require('../../models/URL')
 
 router.get('/', (req, res) => {
     res.render('index')
 })
 
 router.post('/result', (req, res) => {
-    res.render('result')
+    const shortURL = createShortURL()
+    URL.create({
+        origiURL: req.body.URL, 
+        shortURL: shortURL,
+    })
+    res.render('result', { shortURL: `http://localhost:3000/${shortURL}` })
+
 })
 
 module.exports = router
