1ccedca947ca38215d994a65df66f2707f09c1f9
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-10-11 22:05:07 +0800

    嘗試實作驗證重複變數功能2(失敗註解)

--- utils/createShortURL.js
+++ utils/createShortURL.js
@@ -1,4 +1,29 @@
-// //新版程式碼(嘗試進行重複變數驗證) (未完成：卡在非同步程式碼無法理解)
+// //新版程式碼1(嘗試進行重複變數驗證，使用async/await) (未完成：TypeError: createShortURL is not a function)
+// const URL = require('../models/URL')
+// function createGarbled () {
+//     const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+//     const upperCaseLetters = lowerCaseLetters.toUpperCase()
+//     const number = '0123456789'
+//     const allWord = [...lowerCaseLetters, ...upperCaseLetters, ...number]
+//     let result = ''
+//     for (let i = 0 ; i < 5 ; i++) {
+//         result += allWord[Math.floor(Math.random() * allWord.length)]
+//     }
+//     return result
+// }
+
+// module.exports = (async function createShortURL () {
+//     let ShortURL = createGarbled()
+//     let checkShortURL = await URL.findOne({ shortURL: ShortURL }).select('shortURL').lean()
+//     if (!checkShortURL) {
+//         return ShortURL
+//     } else {
+//         return await createShortURL()
+//     }
+// }) ()
+
+
+//新版程式碼(嘗試進行重複變數驗證) (未完成：Error: Cannot find module '../../models/URL')
 // const URL = require('../../models/URL')
 // function createGarbled () {
 //     const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
@@ -18,13 +43,41 @@
 //     if (!checkShortURL) {
 //         return ShortURL
 //     } else {
-//         createShortURL()
+//         return await createShortURL()
 //     }
 // }
 
+// module.exports.createShortURL = createShortURL
+
+
+
+//新版程式碼2(嘗試進行重複變數驗證，使用Promise) (未完成：)
+// const URL = require('../models/URL')
+// function createGarbled () {
+//     const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+//     const upperCaseLetters = lowerCaseLetters.toUpperCase()
+//     const number = '0123456789'
+//     const allWord = [...lowerCaseLetters, ...upperCaseLetters, ...number]
+//     let result = ''
+//     for (let i = 0 ; i < 5 ; i++) {
+//         result += allWord[Math.floor(Math.random() * allWord.length)]
+//     }
+//     return result
+// }
+
+// const createShortURL = new Promise((resolve, reject) => {
+//     let ShortURL = createGarbled()
+//     let checkShortURL = URL.findOne({ shortURL: ShortURL }).select('shortURL').lean()
+//     if (!checkShortURL) {
+//         return resolve(ShortURL)
+//     } else {
+//         return createShortURL()
+//     }
+// })
 // module.exports = createShortURL
 
-//原本的程式碼(未進行重複亂數驗證)
+
+// //原本的程式碼(未進行重複亂數驗證)
 function createShortURL () {
     const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
     const upperCaseLetters = lowerCaseLetters.toUpperCase()
